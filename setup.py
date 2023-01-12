bc6cbf059c611caeb85b03512325644063fb3551
Patrick Minogue <patrick.minogue@gmail.com>
2023-01-12 15:35:25 +0000

    updated version to 0.1.15

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import find_packages, setup
 setup(
     name="smslib",
     packages=find_packages(include=["smslib"]),
-    version="0.1.14",
+    version="0.1.15",
     description="An internal library to send boardon sms messages",
     author="Patrick Minogue",
-    install_requires=['mailjet-rest==1.3.4',
-                      'requests==2.25.1', 'mysql-connector-python==8.0.23'],
+    install_requires=['mailjet-rest==1.3.4', 'requests==2.25.1', 'mysql-connector-python==8.0.23'],
     license="MIT",
 )
