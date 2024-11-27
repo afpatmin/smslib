2dec9f9faee1a710596db786a29ae1fbbb85464c
Patrick Minogue <patrick.minogue@gmail.com>
2024-11-27 22:05:09 +0000

    sms object

--- smslib/text_message.py
+++ smslib/text_message.py
@@ -82,17 +82,21 @@ def __send_smsapi(message, phone, sms_from):
 
     else:
         token = token_response.json().get('access_token')
-        print(token)
-        print(phone)
-        print(sms_from)
+        
+    
         response: requests.Response = requests.post('https://api.linkmobility.com/sms/v1/messages', data=json.dumps({
         'recipient': phone,
         'content': {
             'text': message,
             'options': {
-                'sms.encoding': 'AutoDetect',
-                'sms.sender': sms_from,
-                'sms.obfuscate': 'ContentAndRecipient',
+                'sms': {
+                    'encoding': 'AutoDetect',
+                    'sender': sms_from,
+                    'obfuscate': 'ContentAndRecipient',
+                }
+                #'sms.encoding': 'AutoDetect',
+                #'sms.sender': sms_from,
+                #'sms.obfuscate': 'ContentAndRecipient',
             }
         },
         }), headers={
