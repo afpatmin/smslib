9839172c52a064622fddbeadf81af1e324679302
Patrick Minogue <patrick.minogue@gmail.com>
2024-11-29 13:00:19 +0000

    dont encode

--- smslib/text_message.py
+++ smslib/text_message.py
@@ -83,7 +83,7 @@ def __send_smsapi(message, phone, sms_from):
     else:
         token = token_response.json().get('access_token')
 
-        data = json.dumps({
+        data = {
         'recipient': phone,
         'content': {
             'text': "test",
@@ -95,7 +95,7 @@ def __send_smsapi(message, phone, sms_from):
                 'mode': 'None',                
             }
         },
-        })
+        }
 
         print(data)
         
